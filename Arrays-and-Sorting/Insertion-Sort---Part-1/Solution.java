ca916492259b7165282d16d966feb231c0f6a734
webspellerc5kz <congjiuhuan77382@163.com>
2021-03-26 05:50:52 +0800

    Updating Code

---
+++ Arrays-and-Sorting/Insertion-Sort---Part-1/Solution.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    
+
+    public static void insertIntoSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int valueToSort = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] > valueToSort) {
+                arr[j] = arr[j - 1];
+                j--;
+                printArray(arr);
+            }
+            arr[j] = valueToSort;
+        }
+    }
+    
+    
+/* Tail starts here */
+     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int[] ar = new int[s];
+         for(int i=0;i<s;i++){
+            ar[i]=in.nextInt(); 
+         }
+         insertIntoSorted(ar);
+         printArray(ar);
+    }
+    
+    
+    private static void printArray(int[] ar) {
+      for(int n: ar){
+         System.out.print(n+" ");
+      }
+        System.out.println("");
+   }
+    
+    
+}
+
