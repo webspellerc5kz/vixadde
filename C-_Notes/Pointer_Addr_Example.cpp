f62aa85e664f6fedd63858fbfd8d69b9baef7ab9
webspellerc5kz <congjiuhuan77382@163.com>
2021-04-25 00:41:02 +0800

    Update Pointer_Addr_Example.cpp

--- C-_Notes/Pointer_Addr_Example.cpp
+++ C-_Notes/Pointer_Addr_Example.cpp
@@ -8,12 +8,12 @@ int main() {
 	int y = 2;
 
 	cout << "x = " << x << " y = " << y << endl;
-	swap(&x, &y); // Give me the address of the parameters
+	swap(&x, &y);                                               // Give me the address of the parameters (&a, &b)
 	cout << "Now x = " << x << " y = " << y << endl;
 }
 
 void swap(int* a, int* b) {
-	int temp = *b;
+	int temp = *b;                                              // Points to Address, and swaps the address itself to swap nums.
 	*b = *a;
 	*a = temp;
 }
